42db5eebcdbe857baa9dcd665f7f9d84e9ed60c1
oguzhangur <oguz@arintel.net>
2020-04-28 22:26:54 +0300

    [NEW] home credit default risk data integration

---
+++ data/home_credit_default_risk/process_data.py
@@ -0,0 +1,137 @@
+# %% [markdown]
+# This notebook is a VSCode notebook version of:
+# https://www.kaggle.com/georsara1/lightgbm-all-tables-included-0-778
+# 
+# You could find the data from:
+# https://www.kaggle.com/c/home-credit-default-risk/data
+## All the data files should be in the same directory with this file!
+
+#%% Importing necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix
+from sklearn.feature_selection import VarianceThreshold
+
+#%%
+# Importing data
+data = pd.read_csv('application_train.csv')
+test = pd.read_csv('application_test.csv')
+prev = pd.read_csv('previous_application.csv')
+buro = pd.read_csv('bureau.csv')
+buro_balance = pd.read_csv('bureau_balance.csv')
+credit_card  = pd.read_csv('credit_card_balance.csv')
+POS_CASH  = pd.read_csv('POS_CASH_balance.csv')
+payments = pd.read_csv('installments_payments.csv')
+lgbm_submission = pd.read_csv('sample_submission.csv')
+
+#Feature engineering
+#data['loan_to_income'] = data.AMT_ANNUITY/data.AMT_INCOME_TOTAL
+#test['loan_to_income'] = test.AMT_ANNUITY/test.AMT_INCOME_TOTAL
+
+#%%
+# One-hot encoding of categorical features in data and test sets
+categorical_features = [col for col in data.columns if data[col].dtype == 'object']
+
+one_hot_df = pd.concat([data,test])
+one_hot_df = pd.get_dummies(one_hot_df, columns=categorical_features)
+
+data = one_hot_df.iloc[:data.shape[0],:]
+test = one_hot_df.iloc[data.shape[0]:,]
+
+#%%
+# Pre-processing buro_balance
+buro_grouped_size = buro_balance.groupby('SK_ID_BUREAU')['MONTHS_BALANCE'].size()
+buro_grouped_max = buro_balance.groupby('SK_ID_BUREAU')['MONTHS_BALANCE'].max()
+buro_grouped_min = buro_balance.groupby('SK_ID_BUREAU')['MONTHS_BALANCE'].min()
+
+buro_counts = buro_balance.groupby('SK_ID_BUREAU')['STATUS'].value_counts(normalize = False)
+buro_counts_unstacked = buro_counts.unstack('STATUS')
+buro_counts_unstacked.columns = ['STATUS_0', 'STATUS_1','STATUS_2','STATUS_3','STATUS_4','STATUS_5','STATUS_C','STATUS_X',]
+buro_counts_unstacked['MONTHS_COUNT'] = buro_grouped_size
+buro_counts_unstacked['MONTHS_MIN'] = buro_grouped_min
+buro_counts_unstacked['MONTHS_MAX'] = buro_grouped_max
+
+buro = buro.join(buro_counts_unstacked, how='left', on='SK_ID_BUREAU')
+#%%
+#Pre-processing previous_application
+#One-hot encoding of categorical features in previous application data set
+prev_cat_features = [pcol for pcol in prev.columns if prev[pcol].dtype == 'object']
+prev = pd.get_dummies(prev, columns=prev_cat_features)
+avg_prev = prev.groupby('SK_ID_CURR').mean()
+cnt_prev = prev[['SK_ID_CURR', 'SK_ID_PREV']].groupby('SK_ID_CURR').count()
+avg_prev['nb_app'] = cnt_prev['SK_ID_PREV']
+del avg_prev['SK_ID_PREV']
+
+#%%
+#Pre-processing buro
+#One-hot encoding of categorical features in buro data set
+buro_cat_features = [bcol for bcol in buro.columns if buro[bcol].dtype == 'object']
+buro = pd.get_dummies(buro, columns=buro_cat_features)
+avg_buro = buro.groupby('SK_ID_CURR').mean()
+avg_buro['buro_count'] = buro[['SK_ID_BUREAU', 'SK_ID_CURR']].groupby('SK_ID_CURR').count()['SK_ID_BUREAU']
+del avg_buro['SK_ID_BUREAU']
+
+#%%
+#Pre-processing POS_CASH
+le = LabelEncoder()
+POS_CASH['NAME_CONTRACT_STATUS'] = le.fit_transform(POS_CASH['NAME_CONTRACT_STATUS'].astype(str))
+nunique_status = POS_CASH[['SK_ID_CURR', 'NAME_CONTRACT_STATUS']].groupby('SK_ID_CURR').nunique()
+nunique_status2 = POS_CASH[['SK_ID_CURR', 'NAME_CONTRACT_STATUS']].groupby('SK_ID_CURR').max()
+POS_CASH['NUNIQUE_STATUS'] = nunique_status['NAME_CONTRACT_STATUS']
+POS_CASH['NUNIQUE_STATUS2'] = nunique_status2['NAME_CONTRACT_STATUS']
+POS_CASH.drop(['SK_ID_PREV', 'NAME_CONTRACT_STATUS'], axis=1, inplace=True)
+
+#%%
+#Pre-processing credit_card
+credit_card['NAME_CONTRACT_STATUS'] = le.fit_transform(credit_card['NAME_CONTRACT_STATUS'].astype(str))
+nunique_status = credit_card[['SK_ID_CURR', 'NAME_CONTRACT_STATUS']].groupby('SK_ID_CURR').nunique()
+nunique_status2 = credit_card[['SK_ID_CURR', 'NAME_CONTRACT_STATUS']].groupby('SK_ID_CURR').max()
+credit_card['NUNIQUE_STATUS'] = nunique_status['NAME_CONTRACT_STATUS']
+credit_card['NUNIQUE_STATUS2'] = nunique_status2['NAME_CONTRACT_STATUS']
+credit_card.drop(['SK_ID_PREV', 'NAME_CONTRACT_STATUS'], axis=1, inplace=True)
+
+#%%
+#Pre-processing payments
+avg_payments = payments.groupby('SK_ID_CURR').mean()
+avg_payments2 = payments.groupby('SK_ID_CURR').max()
+avg_payments3 = payments.groupby('SK_ID_CURR').min()
+del avg_payments['SK_ID_PREV']
+
+#%%
+#Join data bases
+data = data.merge(right=avg_prev.reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(right=avg_prev.reset_index(), how='left', on='SK_ID_CURR')
+
+data = data.merge(right=avg_buro.reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(right=avg_buro.reset_index(), how='left', on='SK_ID_CURR')
+
+data = data.merge(POS_CASH.groupby('SK_ID_CURR').mean().reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(POS_CASH.groupby('SK_ID_CURR').mean().reset_index(), how='left', on='SK_ID_CURR')
+
+data = data.merge(credit_card.groupby('SK_ID_CURR').mean().reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(credit_card.groupby('SK_ID_CURR').mean().reset_index(), how='left', on='SK_ID_CURR')
+
+data = data.merge(right=avg_payments.reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(right=avg_payments.reset_index(), how='left', on='SK_ID_CURR')
+
+data = data.merge(right=avg_payments2.reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(right=avg_payments2.reset_index(), how='left', on='SK_ID_CURR')
+
+data = data.merge(right=avg_payments3.reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(right=avg_payments3.reset_index(), how='left', on='SK_ID_CURR')
+
+#%%
+#Remove features with many missing values
+test = test[test.columns[data.isnull().mean() < 0.85]]
+data = data[data.columns[data.isnull().mean() < 0.85]]
+
+#%%
+#Delete customer Id
+del data['SK_ID_CURR']
+del test['SK_ID_CURR']
+
+#%%
+#Export dataframe to pickle
+data.to_pickle('home_credit_default_risk.pkl')
