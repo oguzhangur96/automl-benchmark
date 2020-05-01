8a3273579e7e9ec861741cd58a97414798b22d5a
oguzhangur <oguz@arintel.net>
2020-05-01 20:38:41 +0300

    [FEAT] random seed and validation set to home_credit_default_risk

--- h2o/home_credit_default_risk.py
+++ h2o/home_credit_default_risk.py
@@ -1,5 +1,7 @@
 # %% [markdown]
-# This note
+# This is a simple notebook for H2O AutoMl prediction.
+# MLflow used as tracking tool since experiments take long time complete
+# and it is hard to manage too many experiments. 
 #%%
 # Importing necessary libraries
 import os
@@ -12,8 +14,9 @@ import mlflow.h2o
 
 # %%
 # Initialize h2o server
-h2o.init(max_mem_size='8G')
+h2o.init(max_mem_size='16G')
 
+# Initialize mlflow experiment
 mlflow.set_tracking_uri(f'..{os.sep}mlruns')
 experiment_name = 'automl-benchmark'
 try:
@@ -22,29 +25,45 @@ except:
     experiment = mlflow.get_experiment_by_name(experiment_name)
 mlflow.set_experiment(experiment_name)
 
+# Reading seeds
+seed_path = f'..{os.sep}data{os.sep}seeds.txt'
+seeds = []
+with open(seed_path,mode ='r') as file:
+    for seed in file:
+        seed.strip(r'/n')
+        seeds.append(int(seed))
 
 dataset_name = 'home_credit_default_risk'
 data = pd.read_pickle(f'..{os.sep}data{os.sep}{dataset_name}{os.sep}{dataset_name}.pkl')
 
 #%%
-run_time_secs = 600 
-with mlflow.start_run():
-    train_df = h2o.H2OFrame(data)
-    # Identify predictors and response
-    x = train_df.columns
-    y = "TARGET"
-    x.remove(y)
-    # For binary classification, response should be a factor
-    train_df[y] = train_df[y].asfactor()
-    # Run AutoML for 10 mins with AUC metric
-    aml = H2OAutoML(max_runtime_secs = run_time_secs, 
-                    seed=1, 
-                    stopping_metric='AUC',
-                    sort_metric = 'AUC')
-    aml.train(x=x, y=y, training_frame=train_df)
-    mlflow.log_metric("AUC", aml.leader.auc())
-    mlflow.log_param('seed', 42)
-    mlflow.log_param('run_time', run_time_secs)
-    mlflow.log_param('dataset_name', dataset_name)
-    mlflow.log_param('model_name',aml.leaderboard[0,0])
-    mlflow.h2o.log_model(aml.leader, "model")
+run_time_secs = 600
+target_column = 'TARGET'
+for seed in seeds:
+    with mlflow.start_run():
+        # Split data into two parts (train, valid)
+        train, valid = train_test_split(data, random_state = seed)
+        train_df = h2o.H2OFrame(train)
+        # preparing column data for validation frame it must have same types and names with train frame
+        test_column_names = train_df.columns
+        test_column_types = train_df.types
+        valid_df = h2o.H2OFrame(valid, column_names=test_column_names, column_types=test_column_types)
+        # Identify predictors and response
+        x = train_df.columns
+        x.remove(target_column)
+        # For binary classification, response should be a factor
+        train_df[target_column] = train_df[target_column].asfactor()
+        valid_df[target_column] = valid_df[target_column].asfactor()
+        # Run AutoML for 10 mins with AUC metric
+        aml = H2OAutoML(max_runtime_secs = run_time_secs, 
+                        seed=seed, 
+                        stopping_metric='AUC',
+                        sort_metric = 'AUC')
+        aml.train(x=x, y=target_column, training_frame=train_df, validation_frame=valid_df)
+        
+        mlflow.log_metric('AUC', aml.leader.auc(valid=True))
+        mlflow.log_param('seed', seed)
+        mlflow.log_param('run_time', run_time_secs)
+        mlflow.log_param('dataset_name', dataset_name)
+        mlflow.log_param('model_name',aml.leaderboard[0,0])
+        mlflow.h2o.log_model(aml.leader, 'model')
